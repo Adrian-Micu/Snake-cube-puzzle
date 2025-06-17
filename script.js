d7192493b145909e0da339137873b929e4e20989
Adrian-Micu <danieladrianmicu@gmail.com>
2025-06-17 08:39:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const snakeList = document.getElementById("snakeList");
+const pSnakeListText = document.getElementById("snakeList").innerText;
+const snakeListOpeningBracket = pSnakeListText.indexOf("[");
+const snakeListClosingBracket = pSnakeListText.indexOf("]");
+const snakeList = pSnakeListText.slice(pSnakeListText.indexOf("[") + 1, pSnakeListText.indexOf("]")).split(", ")
+let sumSnake = 0;
+
+for (let i = 0; i < snakeList.length; i++ ) {
+  sumSnake += parseInt(snakeList[i]);
+}
+
+let sanityCheck = sumSnake - (snakeList.length - 1);
+
+console.log(pSnakeListText);
+console.log(snakeListOpeningBracket);
+console.log(snakeListClosingBracket);
 console.log(snakeList);
+console.log(typeof(snakeList));
+console.log(sumSnake);
+console.log(snakeList.length)
+console.log(sanityCheck);
